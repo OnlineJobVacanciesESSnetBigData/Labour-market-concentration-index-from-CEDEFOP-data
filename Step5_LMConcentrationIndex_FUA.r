39bcbdd54800ed3e444e3ce14987e4ff75bb146c
jakobdestat <62878532+jakobdestat@users.noreply.github.com>
2020-12-07 12:27:28 +0100

    Update Step5_LMConcentrationIndex_FUA.r

--- Step5_LMConcentrationIndex_FUA.r
+++ Step5_LMConcentrationIndex_FUA.r
@@ -3,6 +3,18 @@
 #####################################################################################################
 #The geographical unit for this index is the Functional Urban Area (FUA), as defined by Eurostat
 
+# Copyright 2020 DESTATIS
+#  
+# Licensed under the EUPL, Version 1.2 or – as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#  *https://joinup.ec.europa.eu/software/page/eupl5
+#  
+# Unless required by applicable law or agreed to inwriting, software distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Licence for the specific language governing permissions and limitations under the Licence.
+
+
 #adapt paths according to your own file structure
 
 path <- "alldata_june20/"
