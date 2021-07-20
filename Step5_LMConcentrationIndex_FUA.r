1959581a8f9e2a6774addffe95fb29a6ba5cf7a9
jakobdestat <62878532+jakobdestat@users.noreply.github.com>
2021-07-20 10:21:34 +0200

    Updated files with improvements

--- Step5_LMConcentrationIndex_FUA.r
+++ Step5_LMConcentrationIndex_FUA.r
@@ -3,23 +3,12 @@
 #####################################################################################################
 #The geographical unit for this index is the Functional Urban Area (FUA), as defined by Eurostat
 
-# Copyright 2020 DESTATIS
-#  
-# Licensed under the EUPL, Version 1.2 or – as soon as they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
-# You may not use this work except in compliance with the Licence.
-# You may obtain a copy of the Licence at:
-#  *https://joinup.ec.europa.eu/software/page/eupl5
-#  
-# Unless required by applicable law or agreed to inwriting, software distributed under the Licence is distributed on an "AS IS" basis,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the Licence for the specific language governing permissions and limitations under the Licence.
-
-
 #adapt paths according to your own file structure
 
-path <- "alldata_june20/"
-resultspath <- "Results/alldata_june20/"
+path <- "Data/alldata_12_2020/"
+resultspath <- "Results/alldata_12_2020/LMC/"
 
+dir.create(resultspath, showWarnings = FALSE)
 
 options(scipen = 999)
 
@@ -34,55 +23,27 @@ library(stringi)
 library(fst)
 library(readxl)
 
-rm(list = ls())
 
 # load the data and select the relevant subset
 
-dframe <- read_fst(paste0(path,"OJVsample_step1_redux.fst"), c("general_id", "grab_date", "expire_date", "idsector", "qtr", "idesco_level_4", "idregion", "companyname", "idprovince" ), as.data.table = TRUE)
+years <- c(2018, 2019, 2020)
+
+dframe <- data.table()
+
+for (year in years) { 
+  tmp <- read.fst(paste0(path,"OJVsample_step1_redux_", year, ".fst"), as.data.table = TRUE, columns = c("general_id", "grab_date", "expire_date", "idsector", "qtr", "idesco_level_4", "idregion", "companyname", "idprovince" ))
+  dframe <- rbind(dframe, tmp)
+  
+}
+
+rm(tmp)
+setDT(dframe)
 
 dframe <- dframe[startsWith(dframe$idregion, "DE"), ]
-
-# add an industry sector coding which is consistent with that used by the German federal employment agency
-
-nace <- read_excel("NACE.xls")
-nace$charsec <- as.character(nace$charsec)
-
-#distinguish between letter and number codes for sectors in CEDEFOP data:
-
-dframe$numsec <- as.numeric(dframe$idsector)
-dframe$charsec <- dframe$idsector
-dframe$charsec[!is.na(dframe$numsec)] <- NA
-
-#merge the iab classification to both letter and number codes
-
-dframe <- left_join(x = dframe, y = subset(nace, select = c(-charsec)), by = "numsec")
-
-#change the nace table to eliminate duplicates in charsec
-colnames(nace) <- c("naceiab_char",     "charsec"  ,   "numsec"    ,  "Bezeichnung_char")
-nace <- unique(subset(nace, select = -numsec))
-
-dframe <-left_join(x = dframe, y = nace , by = "charsec")
-
-dframe$naceiab[is.na(dframe$naceiab)] <- dframe$naceiab_char[is.na(dframe$naceiab)]
-dframe$Bezeichnung[is.na(dframe$Bezeichnung)] <- dframe$Bezeichnung_char[is.na(dframe$Bezeichnung)]
-
-dframe <- subset(dframe, select = c(-naceiab_char, -numsec, -charsec, -Bezeichnung_char))
-setDT(dframe)
 
 # recode missings to NA
 
 dframe$companyname[dframe$companyname == ""] <- NA
-
-# replace all missing company names with unique strings
-# this turned out to be too arbitrary, replaced with median imputation
-#
-# no <- seq_len(length(dframe$companyname))
-# no <- paste0("missing",no)
-#
-# dframe$companyname[dframe$companyname==""] <- no[dframe$companyname==""]
-#
-# rm(no)
-#
 
 # median imputation
 # count unique obs per occupation, region, quarter, company (preliminary, only for imputation)
@@ -100,6 +61,7 @@ dframe$imp <- paste0("missing", dframe$imp)
 
 dframe$companyname[is.na(dframe$companyname)] <- dframe$imp[is.na(dframe$companyname)]
 
+describe(dframe$companyname)
 
 # read in the shapefiles ======================================
 
@@ -121,14 +83,16 @@ st_geometry(sfile) <- sfile$geometry
 
 #keep only obs with nuts non-missing
 
+describe(dframe$idprovince)
+
 dframe <- subset(dframe, !is.na(idprovince))
 
-#fua <- read.csv("Auxiliary data/kreise_zu_FUAs_raeumlich_verbindung.csv", sep = ";", stringsAsFactors = FALSE)
-fua <- read_excel("Auxiliary data/Kreise_zu_FUAs_raumlich_Verbindung_modified.xlsx")
+fua <- read_excel("Auxiliary data/Kreise_zu_FUAs_raumlich_Verbindung_v2.xlsx")
 
 fua <- subset(fua, select= c("fuacode_si", "NUTS"))
 
 colnames(fua) <- c("fuacode_si", "idprovince")
+
 dframe <- left_join(dframe,fua)
 
 length(unique(dframe$fuacode_si))
@@ -137,7 +101,11 @@ describe(dframe$fuacode_si)
 
 dframe <- dframe[!is.na(dframe$fuacode_si),]
 
+#clean up occupation/region cells with a lack of observations ==========
 
+dframe[, testcount := .N, by = list(idesco_level_4, fuacode_si, qtr)]
+
+dframe <- dframe[testcount > 1]
 
 # Calculate the Herfindahl Hirschman Index =============
 
@@ -166,7 +134,7 @@ hhi <- data.frame()
 for (i in 1:dim(grid)[1]) {
   # count obs per cell and company
   subset <- unique(dframe[idesco_level_4 == grid[i, 1] & fuacode_si == grid[i, 2] & qtr == grid[i, 3], c("idesco_level_4", "fuacode_si", "qtr", "mshare", "ms2", "companyname", "ncount"), with = FALSE])
-
+  
   subset$hhi <- sum(subset$ms2)
 
   subset <- subset[1, !c("companyname") ]
@@ -174,22 +142,49 @@ for (i in 1:dim(grid)[1]) {
   hhi <- rbind(hhi, subset)
 }
 
-save(hhi, file = "HHI_data_FUA.rdata")
+save(hhi, file = paste0(resultspath, "HHI_data_FUA.rdata"))
 
 # load(file = "HHI_data_FUA.rdata")
 
-hhi <- na.omit(hhi)
+setDT(hhi)
 
+#results 
 
-totalmean <- mean(hhi$hhi)
+describe(hhi)
+
+quarters <- unique(hhi$qtr)
+
+for (i in quarters) {
+
+qhhi <- hhi[qtr == i] 
+
+qhhi <- na.omit(qhhi)
+
+totalmean <- mean(qhhi$hhi)
 totalmean
 
-totalmedian <- median(hhi$hhi)
+totalmedian <- median(qhhi$hhi)
 totalmedian  
 
-describe(hhi$hhi)
+q10 <- quantile(qhhi$hhi, 0.1)
+q25 <- quantile(qhhi$hhi, 0.25)
+q75 <- quantile(qhhi$hhi, 0.75)
+q90 <- quantile(qhhi$hhi, 0.90)
 
-ecdf(hhi$hhi)(2500) 
+#describe(hhi$hhi)
+
+threshold25 <- ecdf(qhhi$hhi)(2500) 
+threshold2 <- ecdf(qhhi$hhi)(2000)
+threshold1 <- ecdf(qhhi$hhi)(1000) 
+
+min <- min(qhhi$hhi)
+max <- max(qhhi$hhi)
+
+resmat <- data.frame(variables = c("mean", "median", "q10", "q25", "q75", "q90", "ecdf1000", "ecdf2000", "ecdf2500", "min", "max"), values = c(totalmean, totalmedian, q10, q25, q75, q90, threshold1, threshold2, threshold25, min, max))
+                                   
+write.xlsx(resmat, file = paste0(resultspath, "Resultsmatrix_hhi_fua_", i ,".xlsx"), sheetName = "Sheet1", col.names = TRUE, append = FALSE)
+
+}
 
 hhi <- hhi[, .(idesco_level_4, mshare, ms2, ncount, hhi, wmean = mean(hhi)), by = list(fuacode_si, qtr) ]
 
@@ -200,129 +195,126 @@ hhigeo <- data.table(left_join(hhigeo, sfile, by = "fuacode_si"))
 hhigeo$fuaname <- as.character(hhigeo$fuaname)
 hhigeo$fuaname_en <- as.character(hhigeo$fuaname_en)
 
-
 hhigeo$wmean <- round(hhigeo$wmean)
 
 st_geometry(hhigeo) <- hhigeo$geometry
 
 hhigeo <- st_zm(hhigeo, drop = TRUE, what = "ZM")
 
-hhigeo_q1 <- subset(hhigeo, qtr == "2018-q3")
-hhigeo_q1$label <- paste0(hhigeo_q1$fuaname_en, "\n ", as.character(hhigeo_q1$wmean))
-
-hhigeo_q2 <- subset(hhigeo, qtr == "2018-q4")
-hhigeo_q2$label <- paste0(hhigeo_q2$fuaname_en, "\n ", as.character(hhigeo_q2$wmean))
-
-hhigeo_q3 <- subset(hhigeo, qtr == "2019-q1")
-hhigeo_q3$label <- paste0(hhigeo_q3$fuaname_en, "\n ", as.character(hhigeo_q3$wmean))
+hhigeo$label <- paste0(hhigeo$fuaname_en, "\n ", as.character(hhigeo$wmean))
 
 # Graphs ===========
 
-ggplot(hhigeo_q1) +
-  geom_sf( aes(fill = wmean)) + theme_void() +
-  theme(panel.grid.major = element_line(colour = "transparent")) +
-  labs(title = "Labour market concentration index Q3-2018\naverage over all occupations") +
-  scale_fill_continuous(name = "Labour market concentration index",low="blue", high="orange") +
-  geom_sf_text(aes(label = label), size = 2.5, colour = "black")+
-  geom_sf(data=de,alpha = 0)
-
-ggsave("Results/HHI_q32018_fua.png", width = 15, height = 10, units = "cm")
-
-
-ggplot(hhigeo_q2) +
-  geom_sf(aes(fill = wmean)) + theme_void() +
-  theme(panel.grid.major = element_line(colour = "transparent")) +
-  labs(title = "Labour market concentration index Q4-2018\naverage over all occupations") +
-  scale_fill_continuous(name = "Labour market concentration index", low="blue", high="orange") +
-  geom_sf_text(aes(label = label), size = 2.5, colour = "black")+
-  geom_sf(data=de,alpha = 0)
-
-ggsave("Results/HHI_q42018_fua.png", width = 15, height = 10, units = "cm")
-
-
-ggplot(hhigeo_q3) +
-  geom_sf(aes(fill = wmean)) + theme_void() +
-  theme(panel.grid.major = element_line(colour = "transparent")) +
-  labs(title = "Labour market concentration index Q1-2019\naverage over all occupations") +
-  scale_fill_continuous(name = "Labour market concentration index", low="blue", high="orange") +
-  geom_sf_text(aes(label = label), size = 2.5, colour = "black")+
-  geom_sf(data=de,alpha = 0)
-
-ggsave("Results/HHI_q12019_fua.png", width = 15, height = 10, units = "cm")
+for (i in quarters) {
+  
+  tmp <- subset(hhigeo, qtr == i)
+  
+  ggplot(tmp) +
+    geom_sf( aes(fill = wmean)) + theme_void() +
+    theme(panel.grid.major = element_line(colour = "transparent")) +
+    labs(title = paste0("Labour market concentration index ", i ,"\naverage over all occupations")) +
+    scale_fill_continuous(name = "Labour market concentration index",low="blue", high="orange", limits = c(0,10000)) +
+    geom_sf_text(aes(label = label), size = 2.5, colour = "black")+
+    geom_sf(data=de,alpha = 0)
+  
+  ggsave(paste0(resultspath, "HHI_",i ,"_fua.png"), width = 25, height = 18, units = "cm")
+  
+}
 
 
 # HHI tables by region --------------------------
 
+table <- data.frame( fuacode_si = unique(hhigeo$fuacode_si), fuaname_en =unique(hhigeo$fuaname_en))
 
-table <- data.frame(cbind(hhigeo_q1$fuacode_si, hhigeo_q1$fuaname_en, hhigeo_q1$wmean, hhigeo_q2$wmean, hhigeo_q3$wmean))
-
-table <- table[!is.na(table[,2]),]
-
-colnames(table) <- c("FUA", "Name", "Avg. Q3 2018", "Avg. Q4 2018", "Avg. Q1 2019" )
-
-write.xlsx(table,
-           file = "Results/HHI_FUA.xlsx", sheetName = "Sheet1",
-           col.names = TRUE, append = FALSE
-)
+for (i in quarters) {
+  
+  tmp <- subset(hhigeo, qtr == i, c("fuacode_si", "wmean"))
+  tmp <- st_set_geometry(tmp, NULL)
+  
+  colnames(tmp) <- c("fuacode_si", paste0("Avg. LMC ",i))
+  
+  table <- left_join(table, tmp, by="fuacode_si")
+  
+}
+  write.xlsx(table,
+           file = paste0(resultspath, "HHI_FUA_en.xlsx"), sheetName = "Sheet1",
+           col.names = TRUE, append = FALSE)
 
 # in German ------------------------
 
-table <- data.frame(cbind(hhigeo_q1$fuacode_si, hhigeo_q1$fuaname, hhigeo_q1$wmean, hhigeo_q2$wmean, hhigeo_q3$wmean))
+  
+  table <- data.frame( fuacode_si = unique(hhigeo$fuacode_si), fuaname =unique(hhigeo$fuaname))
+  
+  for (i in quarters) {
+    
+    tmp <- subset(hhigeo, qtr == i, c("fuacode_si", "wmean"))
+    tmp <- st_set_geometry(tmp, NULL)
+    
+    colnames(tmp) <- c("fuacode_si", paste0("Durchschn. LMC ",i))
+    
+    table <- left_join(table, tmp, by="fuacode_si")
+    
+  }
+  write.xlsx(table,
+             file = paste0(resultspath, "HHI_FUA_de.xlsx"), sheetName = "Sheet1",
+             col.names = TRUE, append = FALSE)
 
-table <- table[!is.na(table[,2]),]
+############### Average HHI per year =====================
 
-colnames(table) <- c("FUA", "Name", "Avg. Q3 2018", "Avg. Q4 2018", "Avg. Q1 2019" )
+hhi$year <- as.numeric(substr(hhi$qtr, 1,4))
+  
+for (i in years) {
+    
+  tmp <- subset(hhi, year == i)
 
-write.xlsx(table,
-           file = "Results/HHI_FUA_de.xlsx", sheetName = "Sheet1",
-           col.names = TRUE, append = FALSE
-)
+  hhi_tmean <- tmp[, .(idesco_level_4, mshare, ms2, ncount, hhi, tmean = mean(hhi)), by = list(fuacode_si) ]
+  
+  hhigeo_tmean <- unique(hhi_tmean[, c("fuacode_si", "tmean")])
+  
+  hhigeo_tmean <- data.table(left_join(hhigeo_tmean, sfile, by = "fuacode_si"))
+  
+  hhigeo_tmean$fuaname <- as.character(hhigeo_tmean$fuaname)
+  hhigeo_tmean$fuaname_en <- as.character(hhigeo_tmean$fuaname_en)
 
+  hhigeo_tmean$tmean <- round(hhigeo_tmean$tmean)
+  
+  st_geometry(hhigeo_tmean) <- hhigeo_tmean$geometry
+  
+  hhigeo_tmean <- st_zm(hhigeo_tmean, drop = TRUE, what = "ZM")
+  
+  hhigeo_tmean$label <- paste0(hhigeo_tmean$fuaname_en, "\n ", as.character(hhigeo_tmean$tmean))
+  
+  test <- hhigeo_tmean[is.na(hhigeo_tmean$fuaname),]
+  
+  savetab <- hhigeo_tmean[,c("fuacode_si", "tmean", "fuaname", "fuaname_en")]
+  savetab <- st_set_geometry(savetab, NULL) 
+  savetab$year <- i
+  
+  write.xlsx(savetab, file = paste0(resultspath, "HHI_avg_", i , "_fua.xlsx"), sheetName = "Sheet1", col.names = TRUE, append = FALSE)
 
-############### Average HHI across all quarters =====================
+  ggplot(hhigeo_tmean) +
+    geom_sf( aes(fill = tmean)) + theme_void() +
+    theme(panel.grid.major = element_line(colour = "transparent")) +
+    labs(title = paste0("Labour market concentration index ", i ,"\naverage over occupations and quarters")) +
+    scale_fill_continuous(name = "Labour market concentration index",low="blue", high="orange") +
+    geom_sf_text(aes(label = fuaname_en), size = 2.5, colour = "black")+
+    geom_sf(data=de,alpha = 0)
 
-#hhi_tmean <- hhi %>% group_by(fuacode_si, idesco_level_4) %>% summarise(totalmean = mean(hhi))
-
-
-hhi_tmean <- hhi[, .(idesco_level_4, mshare, ms2, ncount, hhi, tmean = mean(hhi)), by = list(fuacode_si) ]
-
-hhigeo_tmean <- unique(hhi_tmean[, c("fuacode_si", "tmean")])
-
-hhigeo_tmean <- data.table(left_join(hhigeo_tmean, sfile, by = "fuacode_si"))
-
-hhigeo_tmean$fuaname <- as.character(hhigeo_tmean$fuaname)
-hhigeo_tmean$fuaname_en <- as.character(hhigeo_tmean$fuaname_en)
-
-
-hhigeo_tmean$tmean <- round(hhigeo_tmean$tmean)
-
-st_geometry(hhigeo_tmean) <- hhigeo_tmean$geometry
-
-hhigeo_tmean <- st_zm(hhigeo_tmean, drop = TRUE, what = "ZM")
-
-hhigeo_tmean$label <- paste0(hhigeo_tmean$fuaname_en, "\n ", as.character(hhigeo_tmean$tmean))
-
-test <- hhigeo_tmean[is.na(hhigeo_tmean$fuaname),]
-
-ggplot(hhigeo_tmean) +
-  geom_sf( aes(fill = tmean)) + theme_void() +
-  theme(panel.grid.major = element_line(colour = "transparent")) +
-  labs(title = "Labour market concentration index 07.2018 - 03.2019\naverage over occupations and quarters") +
-  scale_fill_continuous(name = "Labour market concentration index",low="blue", high="orange") +
-  geom_sf_text(aes(label = fuaname_en), size = 2.5, colour = "black")+
-  geom_sf(data=de,alpha = 0)
-
-ggsave("Results/maps/HHI_avgfrom_q32018_toq12019_fua.png", width = 20, height = 13.3, units = "cm")
-
+  ggsave(paste0(resultspath, "HHI_avg_", i , "_fua.png"), width = 20, height = 13.3, units = "cm")
+  
+  
 # German #
 
-ggplot(hhigeo_tmean) +
-  geom_sf( aes(fill = tmean)) + theme_void() +
-  theme(panel.grid.major = element_line(colour = "transparent")) +
-  labs(title = "Arbeitsmarkt-konzentrations Index 07.2018 - 03.2019\nDurchschnitt über alle Berufe") +
-  scale_fill_continuous(name = "Arbeitsmarkt-\nkonzentrations Index",low="blue", high="orange") +
-  geom_sf_text(aes(label = fuaname), size = 2.5, colour = "black")+
-  geom_sf(data=de,alpha = 0)
+  ggplot(hhigeo_tmean) +
+    geom_sf( aes(fill = tmean)) + theme_void() +
+    theme(panel.grid.major = element_line(colour = "transparent")) +
+    labs(title = paste0("Arbeitsmarkt-konzentrations Index ", i ,"\nDurchschnitt über alle Berufe")) +
+    scale_fill_continuous(name = "Arbeitsmarkt-\nkonzentrations Index",low="blue", high="orange") +
+    geom_sf_text(aes(label = fuaname), size = 2.5, colour = "black")+
+    geom_sf(data=de,alpha = 0)
 
-ggsave("Results/maps/HHI_avgfrom_q32018_toq12019_fua_de.png", width = 20, height = 13.3, units = "cm")
+  ggsave(paste0(resultspath, "HHI_avg_", i , "_fua_de.png"), width = 20, height = 13.3, units = "cm")
 
+}
+  
+rm(dframe)
